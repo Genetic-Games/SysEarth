98ddf5095cb1736dbfe3186324e350402854c701
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2019-09-22 17:58:00 -0400

    Adding command controller functionality

--- Assets/Scripts/Controllers/CommandController.cs
+++ Assets/Scripts/Controllers/CommandController.cs
@@ -1,10 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using SysEarth.Commands;
+using System.Collections.Generic;
 
 namespace SysEarth.Controllers
 {
     public class CommandController
     {
-        public IList<string> validCommandNames;
+        private IDictionary<string, ICommand> commandMap;
 
+        public CommandController()
+        {
+            commandMap = new Dictionary<string, ICommand>();
+        }
+
+        public bool AddCommand(string commandName, ICommand command)
+        {
+            commandMap.Add(commandName, command);
+            return commandMap.ContainsKey(commandName);
+        }
+
+        public bool RemoveCommand(string commandName)
+        {
+            return commandMap.Remove(commandName);
+        }
+
+        public ICollection<string> GetAvailableCommands()
+        {
+            return commandMap.Keys;
+        }
     }
 }
