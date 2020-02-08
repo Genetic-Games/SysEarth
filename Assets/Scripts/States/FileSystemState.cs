ce3da81652721a7b1356944e7b4acf01380ce43b
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2020-02-08 18:45:07 -0500

    Adding file system state and terminal state classes to manipulate state

---
+++ Assets/Scripts/States/FileSystemState.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Models;
+
+namespace Assets.Scripts.States
+{
+    public class FileSystemState
+    {
+        private Directory _currentDirectory;
+        private Directory _rootDirectory;
+
+        public FileSystemState()
+        {
+            _rootDirectory = new Directory();
+            _currentDirectory = _rootDirectory;
+        }
+
+        public void ChangeCurrentDirectory(Directory target)
+        {
+            _currentDirectory = target;
+        }
+
+        public Directory GetCurrentDirectory(Directory target)
+        {
+            return _currentDirectory;
+        }
+
+        public Permission GetFilePermissions(File target)
+        {
+            return target.Access;
+        }
+
+        public void SetFilePermissions(File targetFile, Permission targetAccess)
+        {
+            targetFile.Access = targetAccess;
+        }
+    }
+}
---
+++ Assets/Scripts/States/TerminalState.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.States
+{
+    public class TerminalState
+    {
+        [SerializeField]
+        private int inputHistoryLimit;
+
+        [SerializeField]
+        private int inputLengthLimit;
+
+        // TODO - Constructor needed here for default limit values?  Particularly for testing?
+
+        private string _currentTerminalInput;
+        private IList<string> _previousTerminalInputs = new List<string>();
+
+        public void SetInputHistoryLimit(int limitValue)
+        {
+            inputHistoryLimit = limitValue;
+        }
+
+        public void SetInputLengthLimit(int limitValue)
+        {
+            inputLengthLimit = limitValue;
+        }
+
+        public string GetCurrentInput()
+        {
+            return _currentTerminalInput;
+        }
+
+        public void SetCurrentInput(string input)
+        {
+            if (input.Length > inputLengthLimit)
+            {
+                _currentTerminalInput = input.Substring(0, inputLengthLimit);
+            }
+
+            _currentTerminalInput = input;
+        }
+
+        public void AddHistoricalInput(string input)
+        {
+            _previousTerminalInputs.Add(input);
+
+            if (_previousTerminalInputs.Count > inputHistoryLimit)
+            {
+                // Remove the oldest inserted element
+                _previousTerminalInputs.RemoveAt(0);
+            }
+        }
+
+        public IList<string> GetPreviousTerminalInputs()
+        {
+            return _previousTerminalInputs;
+        }
+    }
+}
