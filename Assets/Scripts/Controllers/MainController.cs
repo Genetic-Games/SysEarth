7653bfa9ef7a5b66613fa9705d2ae68351e3b478
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2020-02-08 18:44:00 -0500

    Adding note for reference to text parsing

--- Assets/Scripts/Controllers/MainController.cs
+++ Assets/Scripts/Controllers/MainController.cs
@@ -4,6 +4,8 @@ namespace SysEarth.Controllers {
 
     public class MainController : MonoBehaviour
     {
+        // TODO - Check out https://github.com/sprache/Sprache for text parsing
+
         // Start is called before the first frame update
         void Start()
         {
