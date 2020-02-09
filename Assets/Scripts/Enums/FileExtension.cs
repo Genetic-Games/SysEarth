84dd9f97cb70c43bacb039a40deaaf2b6417716c
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2020-02-09 01:20:13 -0500

    Adding FileExtension Enum

---
+++ Assets/Scripts/Enums/FileExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace SysEarth.Enums
+{
+    public enum FileExtension
+    {
+        [Description("")]
+        None,
+
+        [Description("Text")]
+        Txt,
+
+        [Description("Executable")]
+        Exe
+    }
+}
