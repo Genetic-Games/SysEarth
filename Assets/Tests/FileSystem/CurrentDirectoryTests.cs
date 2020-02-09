3bd6903ce81ab2e33a52be79d3a8d9e5202ee188
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2020-02-09 01:21:17 -0500

    Adding significant amount of unit testing done through Unity with NUnit built in (requires asmdef files to pick up these unit tests in Unity)

---
+++ Assets/Tests/FileSystem/CurrentDirectoryTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using SysEarth.States;
+
+namespace SysEarth.Tests.FileSystem
+{
+    public class CurrentDirectoryTests
+    {
+        private FileSystemState _state;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _state = new FileSystemState();
+        }
+
+        [Test]
+        public void NewStateHasCurrentDirectory()
+        {
+            var current = _state.GetCurrentDirectory();
+            Assert.IsNotNull(current);
+        }
+
+        [Test]
+        public void CurrentDirectoryStartsWithNoSubDirectories()
+        {
+            var current = _state.GetCurrentDirectory();
+            Assert.IsNull(current.SubDirectories);
+        }
+
+        [Test]
+        public void CurrentDirectoryStartsWithNoFiles()
+        {
+            var current = _state.GetCurrentDirectory();
+            Assert.IsNull(current.FilesInDirectory);
+        }
+
+        [Test]
+        public void CurrentDirectoryStartsWithNoParentDirectory()
+        {
+            var current = _state.GetCurrentDirectory();
+            Assert.IsNull(current.ParentDirectory);
+        }
+    }
+}
---
+++ Assets/Tests/FileSystem/PermissionsTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using SysEarth.Controllers;
+using SysEarth.States;
+
+namespace SysEarth.Tests.FileSystem
+{
+    public class PermissionsTests
+    {
+        private FileSystemState _state;
+        private PermissionController _permissionController;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _state = new FileSystemState();
+            _permissionController = new PermissionController();
+        }
+
+        [Test]
+        public void RootDirectoryHasReadAccess()
+        {
+            var root = _state.GetRootDirectory();
+            var rootAccess = _permissionController.GetPermissions(root);
+            Assert.IsTrue(rootAccess.Read);
+        }
+
+        [Test]
+        public void RootDirectoryDoesNotHaveWriteAccess()
+        {
+            var root = _state.GetRootDirectory();
+            var rootAccess = _permissionController.GetPermissions(root);
+            Assert.IsFalse(rootAccess.Write);
+        }
+
+        [Test]
+        public void RootDirectoryHasExecuteAccess()
+        {
+            var root = _state.GetRootDirectory();
+            var rootAccess = _permissionController.GetPermissions(root);
+            Assert.IsTrue(rootAccess.Execute);
+        }
+
+        [Test]
+        public void CurrentDirectoryHasReadAccess()
+        {
+            var current = _state.GetCurrentDirectory();
+            var currentAccess = _permissionController.GetPermissions(current);
+            Assert.IsTrue(currentAccess.Read);
+        }
+
+        [Test]
+        public void CurrentDirectoryDoesNotHaveWriteAccess()
+        {
+            var current = _state.GetCurrentDirectory();
+            var currentAccess = _permissionController.GetPermissions(current);
+            Assert.IsFalse(currentAccess.Write);
+        }
+
+        [Test]
+        public void CurrentDirectoryHasExecuteAccess()
+        {
+            var current = _state.GetCurrentDirectory();
+            var currentAccess = _permissionController.GetPermissions(current);
+            Assert.IsTrue(currentAccess.Execute);
+        }
+
+        // TODO - Add more tests here for permissions
+    }
+}
